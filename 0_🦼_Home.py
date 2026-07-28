29d7c75676e7d4fd0e8b794cee04ca5766fe52b0
henglunlin <henglunlin@gmail.com>
2026-07-28 22:12:03 +0800

    Update 0_🦼_Home.py

--- 0_🦼_Home.py
+++ 0_🦼_Home.py
@@ -10,5 +10,6 @@ st.markdown("歡迎來到專屬的自動化工具站！請從 **左側邊欄** �
 st.markdown("### 🛠️ 工具列表")
 st.markdown("* **EDBOM 自動化工具**：將 `EDlist` 與 `BOM` 比對，缺料自動從 `RLClist` 補齊。")
 st.markdown("* **BOM 比對工具**：比對新舊 BOM 表差異，並告訴我你會買日月光。")
+st.markdown("* **SNP File Viewer**：比對SNP file 並且能轉換成`Excel file`，並告訴我你會買日月光。")
 
 st.info("👈 點擊左側選單開始偷懶（X）提升效率（O）！祝您鑽大錢！")
